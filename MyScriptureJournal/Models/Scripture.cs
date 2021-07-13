5a6b33e77e1ff028c97c45dd9d537d8bcc80aa04
Yu-Chun Tung <dyj0183@byui.edu>
2021-07-13 12:06:36 -0600

    added validation and image

--- MyScriptureJournal/Models/Scripture.cs
+++ MyScriptureJournal/Models/Scripture.cs
@@ -11,8 +11,13 @@ namespace MyScriptureJournal.Models
     {
         public int ID { get; set; }
 
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
         [Display(Name = "Scripture Name")]
         public string ScriptureName { get; set; }
+
+        [StringLength(200, MinimumLength = 3)]
+        [Required]
         public string Note { get; set; }
     }
 }
