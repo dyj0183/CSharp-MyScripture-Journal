1a7ebe96de937773ba09c09a5e8b36d90ad4367b
Yu-Chun Tung <dyj0183@byui.edu>
2021-07-13 11:33:32 -0600

    added styleing

--- MyScriptureJournal/Models/Scripture.cs
+++ MyScriptureJournal/Models/Scripture.cs
@@ -2,12 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MyScriptureJournal.Models
 {
     public class Scripture
     {
         public int ID { get; set; }
+
+        [Display(Name = "Scripture Name")]
         public string ScriptureName { get; set; }
         public string Note { get; set; }
     }
