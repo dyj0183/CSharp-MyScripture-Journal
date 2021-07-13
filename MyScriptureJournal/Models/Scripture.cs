53f1be1234e421016175261bee14e5d4f74144b8
Yu-Chun Tung <dyj0183@byui.edu>
2021-07-13 11:06:07 -0600

    added model and scaffolding items

---
+++ MyScriptureJournal/Models/Scripture.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyScriptureJournal.Models
+{
+    public class Scripture
+    {
+        public int ID { get; set; }
+        public string ScriptureName { get; set; }
+        public string Note { get; set; }
+    }
+}
